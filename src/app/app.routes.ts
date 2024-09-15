9938f963d1318c209250b6d6f4f58187fad66c71
Geovane Araujo <geovane-araujo@outlook.com>
2024-09-15 09:23:25 -0300

    ajustes menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,21 +5,27 @@ import { HomeComponent } from './pages/home/home.component';
 import { privateGuard } from './security/guards/private.guard';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { SettingsComponent } from './pages/settings/settings.component';
+import { RepositoryComponent } from './pages/repository/repository.component';
+import { ClassComponent } from './pages/class/class.component';
+import { ActivitiesComponent } from './pages/activities/activities.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: "login", component: LoginComponent, pathMatch: "full", canActivate: [publicGuard] },
 
     {
         path: "home",
-        pathMatch: "full",
         component: HomeComponent,
         canActivateChild: [privateGuard],
         children: [
+            { path: '', redirectTo: 'dashboard', pathMatch: 'full' }, 
             { path: 'dashboard', component: DashboardComponent },
             { path: 'settings', component: SettingsComponent },
-            { path: '', redirectTo: 'dashboard', pathMatch: 'full' }, // Redireciona para dashboard por padrão
+            { path: 'repository', component: RepositoryComponent },
+            { path: 'class', component: ClassComponent },
+            { path: 'activities', component: ActivitiesComponent }
         ]
     },
-    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    
     { path: '**', redirectTo: '/login', pathMatch: 'full' },
 ];
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -2,11 +2,21 @@ import { CommonModule, NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { TooltipModule } from 'primeng/tooltip';
+import { AvatarModule } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [NgClass, RouterLink, TooltipModule,CommonModule, RouterLinkActive],
+  imports: [
+    NgClass, 
+    RouterLink, 
+    TooltipModule,
+    CommonModule, 
+    RouterLinkActive,
+    AvatarModule,
+    AvatarGroupModule
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
@@ -14,17 +24,28 @@ export class SidebarComponent {
 
   public menuItems = [
     {
-      route: "/dashboard",
+      route: "dashboard",
       iconClass: 'pi pi-chart-bar',
       tooltip: 'Dashboard'
     },
     {
-      route: "/dashboard",
-      iconClass: 'pi pi-calendar',
-      tooltip: 'Agenda'
+      route: "activities",
+      iconClass: "pi pi-check-square",
+      tooltip: "Avaliações"
     },
     {
-      route: "/settings",
+      route: "class",
+      iconClass: "pi pi-user-edit",
+      tooltip: "Turmas"
+    },
+    {
+      route: "repository",
+      iconClass: "pi pi-folder-open",
+      tooltip: "Repositório"
+    },
+
+    {
+      route: "settings",
       iconClass: 'pi pi-cog',
       tooltip: 'Configurações'
     }
